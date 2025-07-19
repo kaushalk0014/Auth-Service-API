ca30f1c7857ba4889c8dbf894dd853866ca00691
kaushal kumar <kaushal.k.0014@gmail.com>
2025-07-19 21:09:21 +0530

    initial commit

---
+++ src/test/java/com/auth/service/security/AuthServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.service.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
